d63f6f35cb8f6adde060bfc72b6917fa50bdd7a4
AnteriorCommerce <36268025+AnteriorCommerce@users.noreply.github.com>
2018-06-01 17:47:50 +0530

    Update alphacommercehub.php

--- upload/admin/language/en-gb/extension/payment/alphacommercehub.php
+++ upload/admin/language/en-gb/extension/payment/alphacommercehub.php
@@ -44,4 +44,6 @@ $_['help_total']					 = 'The checkout total the order must reach before this pay
 
 // Error
 $_['error_permission']				 = 'Warning: You do not have permission to modify payment PayPal!';
-$_['error_email']					 = 'E-Mail required!';
+$_['error_email']					 = 'Merchant-id required!';
+$_['error_url']					 = 'Hosted Payment Page URL required!';
+$_['error_user']					 = 'User ID required!';
